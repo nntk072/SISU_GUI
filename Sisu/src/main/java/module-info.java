e7d24860c9e787960fc9f90c55b8aa7cc6932814
Long Nguyen <long.nguyen@tuni.fi>
2023-04-28 23:29:32 +0300

    continuing

--- Sisu/src/main/java/module-info.java
+++ Sisu/src/main/java/module-info.java
@@ -2,7 +2,10 @@ module fi.tuni.prog3.sisu {
     requires javafx.controls;
     requires com.google.gson;
     requires javafx.fxml;
+    requires org.testfx.junit5;
+    requires org.testfx;
 
     opens fi.tuni.prog3.sisu to javafx.fxml;
+    opens org.junit.jupiter.api;
     exports fi.tuni.prog3.sisu;
 }
